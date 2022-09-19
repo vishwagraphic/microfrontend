2c844d9a0972b3def1e3c98ec4330268ec24978f
vishwagraphic <vishwa.graphic@gmail.com>
2022-09-19 16:16:50 -0500

    created container deploy script

---
+++ packages/container/config/webpack.prod.js
@@ -0,0 +1,22 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleContainerPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const domain = process.env.PRODUCTION_DOMAIN;
+
+const prodConfig = {
+  mode: "production",
+  plugins: [
+    new ModuleContainerPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+      },
+      shared: packageJson.dependencies
+    })
+  ]
+};
+
+module.exports = merge(commonConfig, prodConfig);
