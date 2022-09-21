16bc04869d253647223eae3e209c77055ce6ade0
vishwagraphic <vishwa.graphic@gmail.com>
2022-09-21 14:21:58 -0500

    test changes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
     new ModuleContainerPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
+        marketing: `marketing@/marketing/latest/remoteEntry.js`
       },
       shared: packageJson.dependencies
     })
