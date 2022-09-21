9b17773e88ba43ce97d86a4ab1e772b59543ead7
vishwagraphic <vishwa.graphic@gmail.com>
2022-09-21 13:23:31 -0500

    changes for path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
     new ModuleContainerPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
       },
       shared: packageJson.dependencies
     })
