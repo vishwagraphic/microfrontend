5352c1429d858259fd387caeeeead96dc5de2b90
vishwagraphic <vishwa.graphic@gmail.com>
2022-09-21 16:34:07 -0500

    changes for style config

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import {
+  createGenerateClassName,
+  StylesProvider
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "con"
+});
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
